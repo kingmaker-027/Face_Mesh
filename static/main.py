f8456ade969f063422a1e024ed41887821ec7eaa
VISHNU N <57995658+kingmakervn@users.noreply.github.com>
2023-02-04 13:24:38 +0530

    Update main.py

--- static/main.py
+++ static/main.py
@@ -3,7 +3,7 @@ import mediapipe as mp
 import numpy as np
 import csv
 import glob
-# 
+#code 
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_face_mesh = mp.solutions.face_mesh
